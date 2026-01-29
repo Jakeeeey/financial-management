0218eecf31d51fa214242d2f4d57d435f68a0d51
Elijah P. Sadiaza <sadiaza.elijah.pontawe@gmail.com>
2026-01-29 11:14:06 +0800

    Initialize asset management

---
+++ src/modules/financial-management/asset-management/AssetManagementModulePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function AssetManagementModulePage() {
+  return (
+    <div className="flex flex-1 flex-col px-4 ">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4">
+          AssetManagementModulePage
+        </div>
+      </div>
+    </div>
+  );
+}
