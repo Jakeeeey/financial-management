e982a7e47763d5fffdcfd4a218dfaf4be9671397
hatdog14 <kianmendoza4455@gmail.com>
2026-04-22 16:46:45 +0800

    Searchable Combo box

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    allowedDevOrigins: ['msi-andrie'],
+    allowedDevOrigins: ["localhost",
+        "100.70.24.30"
+    ],
 };
 
 export default nextConfig;
