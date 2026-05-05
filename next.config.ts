3b21b4c4c13471983c903cd95a7bacb832b1a097
Jerome Salcedo <jeromelloydsalcedo23@gmail.com>
2026-05-05 13:21:47 +0800

    accounting - sales invoice monitoring
    added view dialog

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     allowedDevOrigins: ["localhost",
-        "100.70.24.30"
+        "100.70.24.30", "100.119.180.15"
     ],
 };
 
