3f9a86c0eaf6ec59e476856a0de959e1187ab497
Rein Solis <reinX244@gmail.com>
2026-06-06 09:42:22 +0800

    Re-added next.config.ts file.

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    /* config options here */
+    allowedDevOrigins: ["localhost",
+        "100.70.24.30", "msi-andrie"
+    ],
+};
+
+export default nextConfig;
